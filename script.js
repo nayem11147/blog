230614fc40af62b14a96f78d41dfb2a2d53e37d5
nayem11147 <nayem11147@gmail.com>
2022-03-11 15:15:37 -0400

    add onclick functions

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const $ = (id) => document.querySelector(id);
+const $_ = (id) => document.querySelectorAll(id);
 
+let blogList = [""];
+
+$_(".save-btn").forEach((el) => {
+  el.addEventListener("click", () => {
+    blogList[0] = $("#editbox").value;
+  });
+});
+
+// function save() {
+//   el.addEventListener("click", () => {
+//     console.log($("#editbox").value);
+//     blogList[0] = $("#editbox").value;
+//     console.log(blogList);
+//   });
+// }
+
+$_(".cancel-btn").forEach((el) => {
+  el.addEventListener("click", () => {
+    $("#editbox").value = blogList[0];
+  });
+});
+
+// function cancel() {
+//   el.addEventListener("click", () => {
+//     $("#editbox").value = blogList[0];
+//   });
+// }
